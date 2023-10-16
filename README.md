5385c71f5a1b50bce26a77c87f128ffde7f133cf
eti-etop <etietopdemas@geekradius.com>
2023-10-16 03:09:03 +0300

    data ingestion research and modules completed

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # publications_prediction
 We are presented with a dataset of one popular social network that includes more than 8.5 million records with meta-information of publications over 13 months (January 2019 to February 2020). Using this data, we will need to predict the number of publications in each 250x250 meter polygon for each hour 4 weeks (28 days) ahead of the last publication in the training set.
+
+# Workflow
+1. Update config.yaml
+2. Update schema.yaml
+3. Update params.yaml
+4. Update entity
+5. Update configuration manager in src/config
+6. Update components
+7. Update pipeline
+8. Update main ochestration
+9. Update app.py
---
+++ main.py
@@ -0,0 +1,35 @@
+from src.predicting_publications import logger
+from src.predicting_publications.pipeline.stage_01_data_ingestion import DataIngestionPipeline
+
+def main():
+    """
+    Main orchestrator function to execute all the pipeline stages in the defined sequence.
+    
+    The function loops through each stage in the execution sequence, initiates, and runs it.
+    Any errors encountered during a stage's execution are logged, and the program is terminated.
+    """
+    
+    # Define the list of pipeline stages to be executed in sequence
+    execution_sequence = [DataIngestionPipeline()]
+
+    for pipeline in execution_sequence:
+        try:
+            # Start and log the current pipeline stage
+            logger.info(f">>>>>> Stage: {pipeline.STAGE_NAME} started <<<<<<")
+            
+            # Execute the `run_pipeline` method of the current pipeline
+            pipeline.run_pipeline()
+            
+            # Log the successful completion of the current pipeline stage
+            logger.info(f">>>>>> Stage {pipeline.STAGE_NAME} completed <<<<<< \n\nx==========x")
+        except Exception as e:
+            # Log any errors encountered during the pipeline's execution
+            logger.exception(f"Error encountered during the {pipeline.STAGE_NAME}: {e}")
+            logger.error("Program terminated due to an error.")
+            
+            # Exit the program with an error status
+            exit(1)
+
+if __name__ == "__main__":
+    # Start the main orchestrator function if the script is run as the main module
+    main()
